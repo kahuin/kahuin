2f2291fdf7544aa3c3ca1fedea193af4d20f9451
Camilo Polymeris <camilo@polymeris.dev>
2019-11-29 16:49:44 -0300

    Add Karma JUnit reporting

--- karma.conf.js
+++ karma.conf.js
@@ -4,12 +4,18 @@ module.exports = function (config) {
         basePath: 'target',
         files: ['karma-test.js'],
         frameworks: ['cljs-test'],
-        plugins: ['karma-cljs-test', 'karma-chrome-launcher'],
+        plugins: ['karma-cljs-test', 'karma-chrome-launcher', 'karma-junit-reporter'],
         colors: true,
         logLevel: config.LOG_INFO,
         client: {
             args: ["shadow.test.karma.init"],
             singleRun: true
+        },
+        reporters: ['progress', 'junit'],
+        junitReporter: {
+            outputDir: "../report",
+            outputFile: "karma-test-results.xml",
+            useBrowserName: false
         }
     })
 };
